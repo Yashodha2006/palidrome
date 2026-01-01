e567e7674a65fe4d0a5d722466fef2cfa90542c8
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-01 22:23:20 +0530

    Update paliandrome.py

--- paliandrome.py
+++ paliandrome.py
@@ -1,15 +1,19 @@
 import sys
 
+# Check if input is given through command line
 if len(sys.argv) > 1:
     word = sys.argv[1]
 else:
-    
     word = input("Enter a word: ")
 
+# Convert to lowercase for accurate comparison
+word = word.lower()
+
+# Reverse the word
 rev = word[::-1]
 
+# Check palindrome
 if word == rev:
     print("It is a palindrome")
 else:
     print("It is not a palindrome")
- 
