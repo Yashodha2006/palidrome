78decbfb2e51f17ce8eee88419d3e105a58a4259
Yashodha2006 <yashodha@gmail.com>
2026-01-01 22:13:02 +0530

    description

--- paliandrome.py
+++ paliandrome.py
@@ -1,17 +1,15 @@
 import sys
 
-# Check if input is given through command line
 if len(sys.argv) > 1:
     word = sys.argv[1]
 else:
-    # Take input from user
+    
     word = input("Enter a word: ")
 
-# Reverse the word
 rev = word[::-1]
 
-# Check palindrome
 if word == rev:
     print("It is a palindrome")
 else:
     print("It is not a palindrome")
+ 
